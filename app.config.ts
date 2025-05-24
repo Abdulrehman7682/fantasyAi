2ab7ee920210052d17471abea9f3a303c6074064
Abdul Rehman <arslangujjar9666@gmail.com>
2025-05-24 21:52:18 +0500

    new

--- app.config.ts
+++ app.config.ts
@@ -36,7 +36,7 @@ export default {
   ],
   android: {
     package: "com.fantasyai.app",
-    versionCode: 7,
+    versionCode: 8,
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#FFFFFF"
